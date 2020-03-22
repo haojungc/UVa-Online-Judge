8fc99a688af64404f15740412a85c783da0a1c78
nickchenchj <nickchenchj@gmail.com>
2020-03-22 15:18:39 +0800

    Solve UVa p100 (the 3n+1 problem)

---
+++ p100.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+
+int get_cycleLength(int n){
+    int count = 1;
+
+    while(n != 1){
+        // odd
+        if(n & 1){
+            n = n * 3 + 1;
+        }
+        // even
+        else{
+            n = n >> 1;
+        }
+
+        count++;
+    }
+
+    return count;
+}
+
+int main(){
+    int n1, n2;
+
+    while(scanf("%d%d", &n1, &n2) == 2){
+        // initialize
+        int t1 = (n1 < n2) ? n1 : n2,   // min
+            t2 = (n2 > n1) ? n2 : n1,   // max
+            max = 0;     
+
+        // Find the maximum cycle length for integers between and including t1 and t2
+        for(int i = t1; i <= t2; i++){
+            int cycleLength = get_cycleLength(i);
+            
+            if(cycleLength > max)
+                max = cycleLength;
+        }
+
+        printf("%d %d %d\n", n1, n2, max);
+    }
+    
+    return 0;
+}
